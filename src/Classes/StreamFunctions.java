8af8fc2db8b934e1b3bd66bc56f206ca6e0c00d8
Sunny Rajpurohit <sunny.rajpurohit@azilen.com>
2023-05-25 13:41:53 +0530

    Update

---
+++ src/Classes/StreamFunctions.java
@@ -0,0 +1,23 @@
+package Classes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFunctions {
+
+    public void functions() {
+        String s1 = "Sunny";
+        String s2 = "Gourav";
+        String s3 = "Ayush";
+        String s4 = "Sunny";
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add(s1);
+        stringList.add(s2);
+        stringList.add(s3);
+        stringList.add(s4);
+
+//        stringList.stream().filter(s -> s.equals("Sunny"))
+    }
+}
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Hello Main Class , Program starts from here => ");
 
-
     }
+
+
 
 
 }
